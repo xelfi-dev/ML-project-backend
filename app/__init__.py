87e5abe32b0a9b603982df1192617fe626dcafa8
xelfi-dev <skrlx123dope@gmail.com>
2024-12-08 19:03:49 +0100

    added some stuff for build

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -9,6 +10,8 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///app.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Optional, disables modification tracking
 
     db.init_app(app)
     migrate.init_app(app, db)
