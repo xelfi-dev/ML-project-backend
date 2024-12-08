abf9d8a3f402afbf4c847ed9cb2f50abc4adf5e0
xelfi-dev <skrlx123dope@gmail.com>
2024-12-08 19:23:03 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///app.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://transactions_k7us_user:8clpBdSCNtfjzzEcGk67FsyONA1TwelP@dpg-ct8tu8btq21c739uogkg-a.oregon-postgres.render.com/transactions_k7us'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Optional, disables modification tracking
 
     db.init_app(app)
